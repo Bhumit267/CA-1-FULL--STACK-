a4c08ed275674cbb6efe2faeaad3d46a8e4daea9
Bhumit267 <bhumitprs15101975@gmail.com>
2025-03-04 14:36:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import TeamMemberCart from './components/TeamMemberCart'
+
+function App() {
+  return (
+    <>
+      <TeamMemberCart/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TeamMemberCart.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TeamMemberCart = () =>{
+    return(
+        <>
+        <h1>Bhumit Peswani</h1>
+        <h2>softwar engg.</h2>
+        <h1>Bhumigaya</h1>
+        <h2>Software engg.</h2>
+        </>
+    
+    )
+}
+
+export default TeamMemberCart;
